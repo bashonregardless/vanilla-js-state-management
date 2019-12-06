bdd2ac9b1cf5aa2fb8bce649ed2dcc3b92e023a2
bashonregardless <harshvardhan0622@gmail.com>
2019-12-06 14:18:08 +0530

    formatted flow algo, implementing TO-DO

--- flow-algo.js
+++ flow-algo.js
@@ -1,15 +1,3 @@
-/* xCell calculation assumption: 
- *
- * if outdegree is 1: only child renders on parent Axis.
- *
- * if outdegree is even: [1..n/2] nodes render to the left of parent Axis and [n/2 + 1..n] render to the right.
- * Nothing renders on parent Axis.
- *
- * if outdegree is odd: [1..floor(n/2)] nodes render to the left of parent Axis
- * and [floor(n/2) + 2..n] nodes render to the right.
- * [floor(n/2) + 1] renders on the parent Axis.
- */
-
 const state = require('./src/js/store/state.js');
 
 var GRAPH_EXPLORER = {};
@@ -17,6 +5,7 @@ var GRAPH_EXPLORER = {};
 GRAPH_EXPLORER.setup = function setup () {
   this.exploredNodes = {};
   this.processingNodesQueue = [];
+  this.queue = [];
 
   this.generatePositions();
 
@@ -28,7 +17,19 @@ GRAPH_EXPLORER.newProcessedNode = function newProcessedNode (depth, xCell) {
   this.xCell = xCell;
 }
 
+/* xCell calculation assumption: 
+ *
+ * if outdegree is 1: only child renders on parent Axis.
+ *
+ * if outdegree is even: [1..n/2] nodes render to the left of parent Axis and [n/2 + 1..n] render to the right.
+ * Nothing renders on parent Axis.
+ *
+ * if outdegree is odd: [1..floor(n/2)] nodes render to the left of parent Axis
+ * and [floor(n/2) + 2..n] nodes render to the right.
+ * [floor(n/2) + 1] renders on the parent Axis.
+ */
 GRAPH_EXPLORER.getXCellVal = function getXCellVal (idx, xCell, outdegree) {
+  /* outdegree is odd */
   if (outdegree & 1) {
 	if ((idx + 1) === Math.floor(outdegree / 2) + 1) {
 	  return xCell;
@@ -37,7 +38,10 @@ GRAPH_EXPLORER.getXCellVal = function getXCellVal (idx, xCell, outdegree) {
 	} else {
 	  return xCell + ( (idx + 1) - ( Math.floor(outdegree / 2) + 1) )
 	}
-  } else {
+  } 
+
+  /* outdegree is even */
+  else {
 	if (idx < outdegree / 2) {
 	  return xCell - (idx + 1);
 	} else {
@@ -47,36 +51,48 @@ GRAPH_EXPLORER.getXCellVal = function getXCellVal (idx, xCell, outdegree) {
 }
 
 GRAPH_EXPLORER.updateExploredNodes = function updateExploredNodes (processingNode, depth) {
-  /* handle termination condition when outgoing edge is incident on a node at lower depth 
+  const { outdegree, connectedNodes } = state.adjL.nodes[processingNode.nodeName];
+  /* TO-DO: handle termination condition when outgoing edge is incident on a node at lower depth 
    * than the depth of the node being processed */
-  if (state.adjL.nodes[processingNode.nodeName].outdegree === 0) {
+  if (outdegree === 0) {
 	return;
   }
 
-  state.adjL.nodes[processingNode.nodeName].connectedNodes.forEach( function (nodeName, idx) {
+  connectedNodes.forEach( function (nodeName, idx) {
 	/* if node hasn't been processed yet, push it to processing queue */
-	if (!Object.prototype.hasOwnProperty.call(this.exploredNodes, nodeName)) {
-	  const xCell = this.getXCellVal(idx, processingNode.xCell, state.adjL.nodes[processingNode.nodeName].outdegree);
+	if (!Object.prototype.hasOwnProperty.call(this.exploredNodes, nodeName) && !this.queue.includes(nodeName)) {
+	  const xCell = this.getXCellVal(idx, processingNode.xCell, outdegree);
 
 	  this.processingNodesQueue.push({
 		nodeName: nodeName, 
 		depth: processingNode.depth + 1,
 		xCell,
 	  });
+	  this.queue.push(nodeName);
 	} else {
 	  this.exploredNodes[nodeName].depth = processingNode.depth + 1;
-	  this.updateExploredNodes({ nodeName, xCell: processingNode.xCell, depth: this.exploredNodes[nodeName].depth });
+	  this.updateExploredNodes({ 
+		nodeName,
+		xCell: processingNode.xCell,
+		depth: this.exploredNodes[nodeName].depth
+	  });
 	}
   }.bind(this))
 }
 
 GRAPH_EXPLORER.generatePositions = function generatePositions () {
   if (state.adjL.root) {
-	this.processingNodesQueue.push({ nodeName: state.adjL.root, depth: 1, xCell: 0 });
+	this.processingNodesQueue.push({ 
+	  nodeName: state.adjL.root,
+	  depth: 1,
+	  xCell: 0
+	});
+	this.queue.push(state.adjL.root);
   }
 
   while (this.processingNodesQueue.length) {
 	const processingNode = this.processingNodesQueue.shift();
+	const qNodeName = this.queue.shift();
 
 	this.updateExploredNodes(processingNode);
 	/* once node has been explored, add it to exploredQueue */
--- src/js/store/state.js
+++ src/js/store/state.js
@@ -5,92 +5,6 @@
 //  ]
 //};
 
-//module.exports = {
-//  root: 'triggerOne',
-//  nodes: {
-//	triggerOne: {
-//	  id: 'triggerOne',
-//	  type: 'trigger',
-//	  text: 'Patient with no visit in last 12 months & no visit scheduled in next 2 months',
-//	  outdegree: 1,
-//	  indegree: 0,
-//	  connectedNodes: [
-//		'actionOne', 
-//	  ]
-//	},
-//	actionOne: {
-//	  id: 'actionOne',
-//	  type: 'action',
-//	  text: 'Text Confirmation and Appointment',
-//	  outdegree: 3,
-//	  indegree: 1,
-//	  connectedNodes: [
-//		'actionFour',
-//		'rescheduled', 
-//		'actionTwo'
-//	  ]
-//	},
-//	actionFour: {
-//	  id: 'actionFour',
-//	  type: 'action',
-//	  text: 'Text Reminder before 1 day',
-//	  outdegree: 1,
-//	  indegree: 3,
-//	  connectedNodes: [
-//		'ifThenOne'
-//	  ]
-//	},
-//	rescheduled: {
-//	  id: 're',
-//	  type: 'label',
-//	  text: 'RESCHEDULED',
-//	  outdegree: 0,
-//	  indegree: 1,
-//	  connectedNodes: []
-//	},
-//	actionTwo: {
-//	  id: 'actionTwo',
-//	  type: 'action',
-//	  text: 'Automated Call Confirmation before 3 days',
-//	  outdegree: 2,
-//	  indegree: 1,
-//	  connectedNodes: [
-//		'actionFour', 
-//		'actionThree'
-//	  ]
-//	},
-//	actionThree: {
-//	  id: 'actionThree',
-//	  type: 'action',
-//	  text: 'Email Confirmation before 1 day',
-//	  outdegree: 1,
-//	  indegree: 1,
-//	  connectedNodes: [
-//		'actionFour',
-//	  ]
-//	},
-//	ifThenOne: {
-//	  id: 'ifThenOne',
-//	  type: 'ifThen',
-//	  text: 'Patient visits after receiving Text Message',
-//	  outdegree: 1,
-//	  indegree: 1,
-//	  connectedNodes: [
-//		'actionFive'
-//	  ]
-//	},
-//	actionFive: {
-//	  id: 'actionFive',
-//	  type: 'action',
-//	  text: 'Call from Care Coordinator',
-//	  outdegree: 0,
-//	  indegree: 1,
-//	  connectedNodes: []
-//	},
-//  }
-//}
-
-
 module.exports = {
   adjL: {
 	root: 'triggerOne',
@@ -109,49 +23,137 @@ module.exports = {
 		id: 'actionOne',
 		type: 'action',
 		text: 'Text Confirmation and Appointment',
+		outdegree: 3,
+		indegree: 1,
+		connectedNodes: [
+		  'actionFour',
+		  'rescheduled', 
+		  'actionTwo'
+		]
+	  },
+	  actionFour: {
+		id: 'actionFour',
+		type: 'action',
+		text: 'Text Reminder before 1 day',
 		outdegree: 1,
-		indegree: 2,
+		indegree: 3,
 		connectedNodes: [
 		  'ifThenOne'
 		]
+	  },
+	  rescheduled: {
+		id: 're',
+		type: 'label',
+		text: 'RESCHEDULED',
+		outdegree: 0,
+		indegree: 1,
+		connectedNodes: []
 	  },
 	  actionTwo: {
 		id: 'actionTwo',
 		type: 'action',
 		text: 'Automated Call Confirmation before 3 days',
-		outdegree: 0,
+		outdegree: 2,
 		indegree: 1,
-		connectedNodes: []
+		connectedNodes: [
+		  'actionFour', 
+		  'actionThree'
+		]
 	  },
 	  actionThree: {
 		id: 'actionThree',
 		type: 'action',
 		text: 'Email Confirmation before 1 day',
-		outdegree: 0,
+		outdegree: 1,
 		indegree: 1,
-		connectedNodes: []
+		connectedNodes: [
+		  'actionFour',
+		]
 	  },
 	  ifThenOne: {
 		id: 'ifThenOne',
 		type: 'ifThen',
 		text: 'Patient visits after receiving Text Message',
-		outdegree: 2,
-		indegree: 1,
-		connectedNodes: [
-		  'filterOne',
-		  'actionTwo'
-		]
-	  },
-	  filterOne: {
-		id: 'filterOne',
-		type: 'filter',
-		text: 'Call from Care Coordinator',
 		outdegree: 1,
 		indegree: 1,
 		connectedNodes: [
-		  'actionThree'
+		  'actionFive'
 		]
+	  },
+	  actionFive: {
+		id: 'actionFive',
+		type: 'action',
+		text: 'Call from Care Coordinator',
+		outdegree: 0,
+		indegree: 1,
+		connectedNodes: []
 	  },
 	}
   }
 }
+
+
+//module.exports = {
+//  adjL: {
+//	root: 'triggerOne',
+//	nodes: {
+//	  triggerOne: {
+//		id: 'triggerOne',
+//		type: 'trigger',
+//		text: 'Patient with no visit in last 12 months & no visit scheduled in next 2 months',
+//		outdegree: 1,
+//		indegree: 0,
+//		connectedNodes: [
+//		  'actionOne', 
+//		]
+//	  },
+//	  actionOne: {
+//		id: 'actionOne',
+//		type: 'action',
+//		text: 'Text Confirmation and Appointment',
+//		outdegree: 1,
+//		indegree: 2,
+//		connectedNodes: [
+//		  'ifThenOne'
+//		]
+//	  },
+//	  actionTwo: {
+//		id: 'actionTwo',
+//		type: 'action',
+//		text: 'Automated Call Confirmation before 3 days',
+//		outdegree: 0,
+//		indegree: 1,
+//		connectedNodes: []
+//	  },
+//	  actionThree: {
+//		id: 'actionThree',
+//		type: 'action',
+//		text: 'Email Confirmation before 1 day',
+//		outdegree: 0,
+//		indegree: 1,
+//		connectedNodes: []
+//	  },
+//	  ifThenOne: {
+//		id: 'ifThenOne',
+//		type: 'ifThen',
+//		text: 'Patient visits after receiving Text Message',
+//		outdegree: 2,
+//		indegree: 1,
+//		connectedNodes: [
+//		  'filterOne',
+//		  'actionTwo'
+//		]
+//	  },
+//	  filterOne: {
+//		id: 'filterOne',
+//		type: 'filter',
+//		text: 'Call from Care Coordinator',
+//		outdegree: 1,
+//		indegree: 1,
+//		connectedNodes: [
+//		  'actionThree'
+//		]
+//	  },
+//	}
+//  }
+//}
